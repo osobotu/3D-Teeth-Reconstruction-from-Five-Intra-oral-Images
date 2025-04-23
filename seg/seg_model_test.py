39ff61a7591ea9f80bf6af1e16ab9c4e5996791b
Stephen Obotu Oduh <osobotu@gmail.com>
2025-04-23 15:14:16 +0200

    FIX: output shape of 512 by 512

--- seg/seg_model_test.py
+++ seg/seg_model_test.py
@@ -19,15 +19,15 @@ def CascadeConv2D(x, filters, conv_times, k_size=3, leaky_rate=0.1, dila=1):
         x = LeakyConv2D(x, filters, k_size, leaky_rate, dila)
     return x
 
-def DenseASPP(x, filters, d_rates=[3, 6, 12, 18, 24], leaky_rate=0.1):
-    concat_feats = [x]
-    for i, rate in enumerate(d_rates):
-        x = layers.Conv2D(filters, kernel_size=3, padding='same', dilation_rate=rate, activation='linear')(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.LeakyReLU(alpha=leaky_rate)(x)
-        concat_feats.append(x)
-        x = layers.concatenate(concat_feats)
-    return x
+# def DenseASPP(x, filters, d_rates=[3, 6, 12, 18, 24], leaky_rate=0.1):
+#     concat_feats = [x]
+#     for i, rate in enumerate(d_rates):
+#         x = layers.Conv2D(filters, kernel_size=3, padding='same', dilation_rate=rate, activation='linear')(x)
+#         x = layers.BatchNormalization()(x)
+#         x = layers.LeakyReLU(alpha=leaky_rate)(x)
+#         concat_feats.append(x)
+#         x = layers.concatenate(concat_feats)
+#     return x
 
 def SeparableConv2D(x, filters, dila=1, leaky_rate=0.1):
     x = layers.DepthwiseConv2D(
@@ -72,30 +72,130 @@ def DenseASPP_UNet(shape, kern_size=3, filters=[64, 128, 256, 512, 1024]):
     return model
 
 
-def DenseASPP_ResNet(shape=(512, 512, 3), filters=[64, 128, 256, 512, 1024], kern_size=3):
+# def DenseASPP_ResNet(shape=(512, 512, 3), filters=[64, 128, 256, 512, 1024], kern_size=3):
+#     outputShape = shape[:2]
+#     conv_times = 2
+#     inp = layers.Input(shape)
+#     backbone = ResNet50(include_top=False, weights="imagenet", input_tensor=inp)
+#     skip_connections = [
+#         backbone.get_layer("conv1_relu").output,
+#         backbone.get_layer("conv2_block3_out").output,
+#         backbone.get_layer("conv3_block4_out").output,
+#         backbone.get_layer("conv4_block6_out").output,
+#     ]
+#     x = backbone.output  # Deepest feature map
+#     x = DenseASPP(x, filters[-1])  # Apply DenseASPP module
+
+#     # Decoder
+#     for i in reversed(range(len(skip_connections))):
+#         f = filters[i]
+#         x = layers.Conv2DTranspose(f, kernel_size=2, strides=2, padding="valid")(x)
+#         x = layers.Concatenate()([x, skip_connections[i]])
+#         x = CascadeConv2D(x, f, conv_times, kern_size, leaky_rate=0.1, dila=1)
+
+#     x = LeakyConv2D(x, filters=1, k_size=1, leaky_rate=0.1, dila=1)
+#     print(x.shape)
+#     # x = layers.Reshape(outputShape)(x)
+#     model = keras.Model(inp, x, name="DenseASPP-ResNet")
+#     return model
+
+def ResNetBlock(x, filters, kernel_size=3, strides=1, leaky_rate=0.1):
+    """
+    A standard ResNet block with two convolutions and a skip connection
+    """
+    shortcut = x
+    
+    # First convolution
+    x = layers.Conv2D(filters, kernel_size, strides=strides, padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.LeakyReLU(alpha=leaky_rate)(x)
+    
+    # Second convolution
+    x = layers.Conv2D(filters, kernel_size, padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    
+    # Skip connection with projection if needed
+    if strides > 1 or shortcut.shape[-1] != filters:
+        shortcut = layers.Conv2D(filters, 1, strides=strides, padding='same')(shortcut)
+        shortcut = layers.BatchNormalization()(shortcut)
+    
+    # Add skip connection
+    x = layers.add([x, shortcut])
+    x = layers.LeakyReLU(alpha=leaky_rate)(x)
+    
+    return x
+
+def EncoderBlock(x, filters, blocks=2, strides=2, leaky_rate=0.1):
+    """
+    ResNet encoder block with multiple ResNet blocks
+    """
+    # First block with stride
+    x = ResNetBlock(x, filters, strides=strides, leaky_rate=leaky_rate)
+    
+    # Additional blocks
+    for _ in range(1, blocks):
+        x = ResNetBlock(x, filters, leaky_rate=leaky_rate)
+    
+    return x
+
+def DenseASPP(x, filters, d_rates=[3, 6, 12, 18, 24], leaky_rate=0.1):
+    concat_feats = [x]
+    for i, rate in enumerate(d_rates):
+        x = layers.Conv2D(filters, kernel_size=3, padding='same', dilation_rate=rate, activation='linear')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU(alpha=leaky_rate)(x)
+        concat_feats.append(x)
+        x = layers.concatenate(concat_feats)
+    return x
+
+def DenseASPP_ResNet(shape, filters=[64, 128, 256, 512], aspp_filters=256):
+    """
+    A ResNet-based encoder with DenseASPP module and decoder for segmentation
+    """
     outputShape = shape[:2]
-    conv_times = 2
-    inp = layers.Input(shape)
-    backbone = ResNet50(include_top=False, weights="imagenet", input_tensor=inp)
-    skip_connections = [
-        backbone.get_layer("conv1_relu").output,
-        backbone.get_layer("conv2_block3_out").output,
-        backbone.get_layer("conv3_block4_out").output,
-        backbone.get_layer("conv4_block6_out").output,
-    ]
-    x = backbone.output  # Deepest feature map
-    x = DenseASPP(x, filters[-1])  # Apply DenseASPP module
-
-    # Decoder
-    for i in reversed(range(len(skip_connections))):
-        f = filters[i]
-        x = layers.Conv2DTranspose(f, kernel_size=2, strides=2, padding="valid")(x)
-        x = layers.Concatenate()([x, skip_connections[i]])
-        x = CascadeConv2D(x, f, conv_times, kern_size, leaky_rate=0.1, dila=1)
-
-    x = LeakyConv2D(x, filters=1, k_size=1, leaky_rate=0.1, dila=1)
-    print(x.shape)
-    # x = layers.Reshape(outputShape)(x)
-    model = keras.Model(inp, x, name="DenseASPP-ResNet")
+    input_tensor = layers.Input(shape)
+    
+    # Initial convolution
+    x = layers.Conv2D(64, 7, strides=2, padding='same')(input_tensor)
+    x = layers.BatchNormalization()(x)
+    x = layers.LeakyReLU(0.1)(x)
+    x = layers.MaxPooling2D(3, strides=2, padding='same')(x)
+    
+    # Store encoder outputs for skip connections
+    skip_connections = []
+    
+    # ResNet encoder blocks
+    for i, f in enumerate(filters):
+        blocks = 2 if i < len(filters) - 1 else 3  # More blocks in the last stage
+        strides = 1 if i == 0 else 2  # No downsampling in the first block after initial conv
+        skip_connections.append(x)
+        x = EncoderBlock(x, f, blocks=blocks, strides=strides)
+    
+    # Apply DenseASPP module
+    x = DenseASPP(x, aspp_filters)
+    
+    # Decoder with skip connections
+    for i in range(len(filters)-1, -1, -1):
+        upsampling_factor = 2
+        
+        # Upsampling
+        x = layers.Conv2DTranspose(filters[i], 2, strides=upsampling_factor, padding='same')(x)
+        
+        # Skip connection
+        x = layers.concatenate([x, skip_connections[i]])
+        
+        # Refine features
+        x = ResNetBlock(x, filters[i])
+    
+    # Final upsampling to input resolution
+    x = layers.Conv2DTranspose(64, 2, strides=4, padding='same')(x)
+    x = layers.LeakyReLU(0.1)(x)
+    
+    # Output layer
+    x = layers.Conv2D(1, 1, activation='linear')(x)
+    x = layers.Reshape(outputShape)(x)
+    
+    # Create model
+    model = keras.Model(input_tensor, x, name="DenseASPP-ResNet")
+    
     return model
-
